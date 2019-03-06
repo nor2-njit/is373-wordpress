f5d7724577362df177f23a7f11fed91fca9c3532
nor2-njit <nor2@njit.edu>
2019-03-06 13:35:26 -0500

    changed functionality of plugin (adds footer text)

--- wp-content/plugins/is373-simple-plugin/is373-simple-plugin.php
+++ wp-content/plugins/is373-simple-plugin/is373-simple-plugin.php
@@ -10,8 +10,9 @@
  * Author: Nicholas R.
  */
 
-function nor_modify_read_more_link() {
-	return '<a class="more-link" href="' . get_permalink() . '"> Click to Read!</a>';
-}
 
-add_filter( 'the_content_more_link', 'nor_modify_read_more_link');
+add_action( 'the_content', 'footer_text' );
+
+function footer_text($content) {
+	return $content .= '<p>This article was written by two NJIT students who are learning about WordPress.</p>';
+}
